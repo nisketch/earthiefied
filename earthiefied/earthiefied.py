57c791e59bda65c4460ed370526cd1cab7ac60ed
Nimrod <nimrodogoro@gmail.com>
2024-04-28 17:29:13 +0300

    commit

--- earthiefied/earthiefied.py
+++ earthiefied/earthiefied.py
@@ -1,1 +1,9 @@
 """Main module."""
+from typing import Self
+import ipyleaflet
+
+class  map(ipyleaflet.Map):
+    
+ def _init_(self, kwargs):
+    super()._init_(**kwargs)
+    Self.add_control(ipyleaflet.LayersControl())
